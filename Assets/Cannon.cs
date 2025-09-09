635fb99f9a9a408411ac9c646799b1ee1d265981
LorenzoPrince <lorenzoprince222@gmail.com>
2025-09-09 00:36:27 -0300

    ForceController
    Controlamos la fuerza con la cual dispara el cañon la bala

--- Assets/Cannon.cs
+++ Assets/Cannon.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Cannon : MonoBehaviour
 {
@@ -12,9 +13,19 @@ public class Cannon : MonoBehaviour
     [SerializeField] private Slider angleSliderz;
 
 
+    [SerializeField] private Slider forceSlider;
+    [SerializeField] private TMP_Text forceText;
+
+
     void Start()
     {
-        
+        // Asignar valor inicial si el slider está conectado
+        if (forceSlider != null)
+        {
+            forceSlider.value = shootForce;
+            forceSlider.onValueChanged.AddListener(UpdateShootForce);
+            UpdateShootForce(forceSlider.value); // Mostrar texto inicial
+        }
     }
 
     // Update is called once per frame
@@ -45,5 +56,13 @@ public class Cannon : MonoBehaviour
         Vector3 currentRotation = transform.localRotation.eulerAngles;
         transform.localRotation = Quaternion.Euler(currentRotation.x, currentRotation.y, anglez);
     }
+    public void UpdateShootForce(float newForce)
+    {
+        shootForce = newForce;
 
+        if (forceText != null)
+        {
+            forceText.text = "Fuerza: " + shootForce.ToString("F0");
+        }
+    }
 }
